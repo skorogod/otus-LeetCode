5c61c06461ed47dca7c1c40e3a937660f53cd7d4
Sergey <mail@vasya.com>
2024-03-24 16:56:56 +0300

    deleted db connection

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
-import { dataSource } from "./db/dataSource";
 import { swaggerSpec } from "./api/swagger/swagger";
 import swaggerUi from "swagger-ui-express";
 
 import { app } from "./app";
 
-dataSource
-  .initialize()
-  .then(() => {
-    console.log("Data Source has been initialized");
-  })
-  .catch((err) => {
-    console.log("Error during Data Source initiakization", err);
-  });
 
 app.listen(5000, () => {
   console.log("[Server]: Server is running at http://localhost:5000");
